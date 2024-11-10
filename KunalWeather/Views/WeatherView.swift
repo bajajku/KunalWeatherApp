4935c13c6e77c96f34189ca9d8f35d6b1081de44
Kunal <bajajku@sheridancollege.ca>
2024-11-10 15:33:29 -0500

    Basic weather implementation done, need integrate getting user's location and displaying weather based ont that.

--- KunalWeather/Views/WeatherView.swift
+++ KunalWeather/Views/WeatherView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct WeatherView: View {
+    @StateObject private var viewModel = WeatherViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            if let weather = viewModel.weather {
+                Text("Location: \(weather.location.name), \(weather.location.region), \(weather.location.country)")
+                    .font(.headline)
+                
+                Text("Temperature: \(weather.current.temp_c)°C (\(weather.current.temp_f)°F)")
+                Text("Condition: \(weather.current.condition.text)")
+                Text("Humidity: \(weather.current.humidity)%")
+                Text("Feels Like: \(weather.current.feelslike_c)°C (\(weather.current.feelslike_f)°F)")
+                
+            } else {
+                Text("Fetching weather data...")
+            }
+        }
+        .padding()
+        .onAppear {
+            viewModel.getWeather(for: "Mississauga", aqi: "yes"){ result in
+                switch result {
+                case .success:
+                    print("Weather data fetched successfully")
+                case .failure(let error):
+                    print("Error fetching weather data: \(error)")
+                }
+            }
+        }
     }
 }
 
